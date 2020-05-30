87b03cdc5cfb9af3adc51c2d120cff6839b81297
wolfgang <wmhuber2@illinois.edu>
2020-05-30 12:45:21 -0500

    Fixed Number of dice rolled

--- DiceRules.py
+++ DiceRules.py
@@ -10,6 +10,6 @@ async def on_message(Data, channels, server, payload):
     if payload['Content'][0] != '!': return
     diceInfo = payload['Content'][1:].split('d')
     if len(diceInfo) == 2:
-        randNum = numpy.random.randint(1, int(diceInfo[1]), int(diceInfo[0]))
+        randNum = numpy.random.randint(1, int(diceInfo[1])+1, int(diceInfo[0]))
         await channels[server.id][payload['Channel']].send(str(sum(randNum))+' : '+str(randNum).replace(' ',', '))
 
