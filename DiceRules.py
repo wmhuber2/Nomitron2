bf8b29da3ee47d596ae9e32188c6c11ed6e09993
wolfgang <wmhuber2@illinois.edu>
2020-05-29 20:38:52 -0500

    Automatic Backup System Added, Modifired Admin.py to AdminRules.py for compatability with new discordBot scirpt.

---
+++ DiceRules.py
@@ -0,0 +1,15 @@
+#
+# Blank Module For Discord Bot
+################################
+import pickle, sys, numpy
+"""
+Main Run Function On Messages
+"""
+async def on_message(Data, channels, server, payload):
+    message = payload['raw']
+    if payload['Content'][0] != '!': return
+    diceInfo = payload['Content'][1:].split('d')
+    if len(diceInfo) == 2:
+        randNum = numpy.random.randint(1, int(diceInfo[1]), int(diceInfo[0])+1)
+        await channels[server.id][payload['Channel']].send(str(sum(randNum))+' - '+str(randNum))
+
