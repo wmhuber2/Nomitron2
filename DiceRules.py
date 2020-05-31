ae70c70108cf370e79f43c9d74163c73735f5a9b
dANIEL <dmouscher@gmail.com>
2020-05-30 19:19:37 -0500

    New function format

--- DiceRules.py
+++ DiceRules.py
@@ -5,10 +5,9 @@ import pickle, sys, numpy
 """
 Main Run Function On Messages
 """
-async def on_message(Data, channels, server, payload):
-
-    if payload['Content'][0] != '!': return
-    diceInfo = payload['Content'][1:].split('d')
+async def roll(Data, channels, server, payload, *text):
+    argv = text[0]
+    diceInfo = argv[1].split('d')
     if len(diceInfo) == 2:
         randNum = numpy.random.randint(1, int(diceInfo[1])+1, int(diceInfo[0]))
         await channels[server.id][payload['Channel']].send(str(sum(randNum))+' : '+str(randNum).replace(' ',', '))
--- Rules.py
+++ Rules.py
@@ -6,86 +6,91 @@ import pickle, sys, urllib
 """
 Main Run Function On Messages
 """
-async def on_message(Data, channels, server, payload):
 
-    # Do Stuff Here
+async def rule(Data, channels, server, payload, *text):
+    argv = text[0]
+
+    rulequery = int(argv[1])
+    if(rulequery not in Data[server.id]['RuleList'].keys()):
+        await channels[server.id][payload['Channel']].send("I couldn't find that rule.")
+    else:
+        print("Found Rule", rulequery)
+        answer = Data[server.id]['RuleList'][rulequery]
+        response = ""
+        for paragraph in answer.split("\n\n"):
+            paragraph = paragraph.replace('\\xe2\\x95\\x9e', ' ')
+            paragraph = paragraph.replace('\\xe2\\x95\\x90', ' ')
+            paragraph = paragraph.replace('\\xe2\\x95\\xa1', ' ')
+            paragraph = paragraph.replace('\\xe2\\x94\\x80', ' ')
+            if(len(response) + len(paragraph) + 6 > 1850):
+                #print(response)
+                await channels[server.id][payload['Channel']].send(response)
+                response = ""
+            if len(paragraph) >1900:
+                print('Error: Paragraph too long!!! Length: ', len(paragraph))
+                print(paragraph)
+            else:
+                response = response + "\n\n" + paragraph
+        #print(response)
+        await channels[server.id][payload['Channel']].send(response)
+
+async def find(Data, channels, server, payload, *text):
+    argv = text[0]
+    query = ' '.join(argv[1:]).lower()
     message = payload['raw']
-    splitPayload = payload['Content'].split()
-    if len(splitPayload) == 0: pass
 
-    elif(splitPayload[0] == "!rule" and len(splitPayload) == 2):
-        rulequery = int(splitPayload[1])
-        if(rulequery not in Data[server.id]['RuleList'].keys()):
-            await channels[server.id][payload['Channel']].send("I couldn't find that rule.")
-        else:
-            print("Found Rule", rulequery)
-            answer = "Rule " + Data[server.id]['RuleList'][rulequery]
-            response = ""
-            for paragraph in answer.split("\n\n"):
-                paragraph = paragraph.replace('\\xe2\\x95\\x9e', ' ')
-                paragraph = paragraph.replace('\\xe2\\x95\\x90', ' ')
-                paragraph = paragraph.replace('\\xe2\\x95\\xa1', ' ')
-                paragraph = paragraph.replace('\\xe2\\x94\\x80', ' ')
-                if(len(response) + len(paragraph) + 6 > 1850):
-                    #print(response)
-                    await channels[server.id][payload['Channel']].send(response)
-                    response = ""
-                if len(paragraph) >1900:
-                    print('Error: Paragrpah too long!!! Length: ', len(paragraph))
-                    print(paragraph)
-                else:
-                    response = response + "\n\n" + paragraph
-            #print(response)
-            await channels[server.id][payload['Channel']].send(response)
+    if query[ 0] == '"': query = query[1:  ]
+    if query[-1] == '"': query = query[ :-1]
+    print (query)
+    if len(query) <= 3: await message.channel.send("Must Search Words Longer Then 3 Letters")
+    else:
+        found = False
+        rulecount = 5
+        for rule in Data[server.id]['RuleList'].keys():
+            low = Data[server.id]['RuleList'][rule].lower()
 
-    elif (splitPayload[0] == "!search" or splitPayload[0] == "!find"or splitPayload[0] == "!f"):
-        text = ' '.join(splitPayload[1:]).lower()
-        if text[0] == '"': text = text[1:]
-        if text[-1] == '"': text = text[:-1]
-        print (text)
-        if len(text) <= 3: await message.channel.send("Must Search Words Longer Then 3 Letters")
-        else:
-            found = False
-            rulecount = 5
-            for rule in Data[server.id]['RuleList'].keys():
-                low = Data[server.id]['RuleList'][rule].lower()
+            if query in low and rulecount <= 0:
 
-                if text in low and rulecount <= 0:
+                await message.channel.send("and in rule "+str(rule))
+            if query in low and rulecount >= 0:
+                rulecount-=1
+                isIn = 1
+                count = 2
+                initIndex = 0
+                msg = '`'+str(rule)+':`\n'
+                while isIn and count > 0:
+                    found = True
+                    try:
+                        index = low[initIndex:].index(query)
+                        index += initIndex
 
-                    await message.channel.send("and in rule "+str(rule))
-                if text in low and rulecount >= 0:
-                    rulecount-=1
-                    isIn = 1
-                    count = 2
-                    initIndex = 0
-                    msg = '`'+str(rule)+':`\n'
-                    while isIn and count > 0:
-                        found = True
-                        try:
-                            index = low[initIndex:].index(text)
-                            index += initIndex
+                        initIndex = index + len(query)
 
-                            initIndex = index + len(text)
+                        boundLower = index - 40
+                        if boundLower < 0:boundLower = 0
 
-                            boundLower = index - 40
-                            if boundLower < 0:boundLower = 0
+                        boundUpper = index + 120
+                        if boundUpper >= len(low): boundUpper = len(low)-1
 
-                            boundUpper = index + 120
-                            if boundUpper >= len(low): boundUpper = len(low)-1
+                        msg +=('\t...'\
+                              +Data[server.id]['RuleList'][rule][boundLower:index]\
+                              +'**'+ Data[server.id]['RuleList'][rule][index:index+len(query)]\
+                              +'**'+ Data[server.id]['RuleList'][rule][index+len(query):boundUpper]\
+                              +'...').replace('\n','  ')+'\n\n'
+                    except ValueError:
+                        isIn = 0
+                    count -= 1
+                if count <= 0:
+                    msg += '...and more...'
+                await message.channel.send(msg)
+        if not found:
+            await message.channel.send("Couldn't Find A Match For "+query)
 
-                            msg +=('\t...'\
-                                  +Data[server.id]['RuleList'][rule][boundLower:index]\
-                                  +'**'+ Data[server.id]['RuleList'][rule][index:index+len(text)]\
-                                  +'**'+ Data[server.id]['RuleList'][rule][index+len(text):boundUpper]\
-                                  +'...').replace('\n','  ')+'\n\n'
-                        except ValueError:
-                            isIn = 0
-                        count -= 1
-                    if count <= 0:
-                        msg += '...and more...'
-                    await message.channel.send(msg)
-            if not found:
-                await message.channel.send("Couldn't Find A Match For "+text)
+async def f(Data, channels, server, payload, *text):
+	await find(Data, channels, server, payload, *text)
+
+async def search(Data, channels, server, payload, *text):
+	await find(Data, channels, server, payload, *text)
 
 """
 Setup Log Parameters and Channel List And Whatever You Need to Check on a Bot Reset.
@@ -94,15 +99,17 @@ Handles Change In Server Structure and the like. Probably Can Leave Alone.
 async def setup(Data, channels, server, payload):
     # Do Stuff Here
     Data[server.id]['RuleList'] = {}
-    with urllib.request.urlopen('https://raw.githubusercontent.com/dmouscher/nomic/master/Game_4/rules-s.md') as response:
+    with urllib.request.urlopen('https://raw.githubusercontent.com/dmouscher/nomic/master/Game_4/rules-s.txt') as response:
         rules = response.read().decode("utf-8")
-        ruletxt = rules.split("\n## ")[1:]
+        ruletxt = rules.split("\n----------------------------------------------------------------------\n")[1:]
         for rule in ruletxt:
             try:
-                rule = rule.strip().split('\n\n',1)
-                rulenum = int(rule[0][0:3])
-                Data[server.id]['RuleList'][rulenum] = rule[1].replace('&nbsp;', ' ').replace('\\n', '\n')
+                #rule = rule.strip().split('\n\n',1)
+                rulenum = int(rule.split(' ')[1])
+
+                with urllib.request.urlopen('https://raw.githubusercontent.com/dmouscher/nomic/master/Game_4/rules/txt/' + str(rulenum) + '.txt') as ruleresponse:
+                    print("Loading Rule " + str(rulenum))
+                    Data[server.id]['RuleList'][rulenum] = ruleresponse.read().decode("utf-8")#[1].replace('&nbsp;', ' ').replace('\\n', '\n')
             except:
                 print('ERROR')
                 print(rule)
-
